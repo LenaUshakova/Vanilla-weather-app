645c49116193ea7936f4e24c36ca403a33a28b58
LenaUshakova <4elena.u@gmail.com>
2022-11-04 12:49:04 -0400

    Added uiUpdate function
    to functions reduced into one new  uiUpdate

--- src/index.js
+++ src/index.js
@@ -31,45 +31,30 @@ currentTime.innerHTML = `${hours}:${minutes}`;
 // City Search + Current Temperature
 function getCityName(coords) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=${units}`;
-  return axios.get(apiUrl).then(function (response) {
-    let cityName = response.data.name;
-    let curTemp = Math.round(response.data.main.temp);
-    cityLabel.innerHTML = cityName;
-    tempValue.innerHTML = curTemp;
-    console.log("response", response.data);
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML =
-      Math.round(response.data.wind.speed * 3.6 * 1) / 1;
-    document.querySelector("#weather-description").innerHTML =
-      response.data.weather[0].main;
-    document.querySelector(".max-temperature").innerHTML = Math.round(
-      response.data.main.temp_max
-    );
-    document.querySelector(".min-temperature").innerHTML = Math.round(
-      response.data.main.temp_min
-    );
-  });
+  return axios.get(apiUrl).then(uiUpdate);
 }
 
 function getCityWeather(cityName) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
-  return axios.get(apiUrl).then(function (response) {
-    let curTemp = Math.round(response.data.main.temp);
-    console.log("response", response.data);
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML =
-      Math.round(response.data.wind.speed * 3.6 * 1) / 1;
-    document.querySelector("#weather-description").innerHTML =
-      response.data.weather[0].main;
-    document.querySelector(".max-temperature").innerHTML = Math.round(
-      response.data.main.temp_max
-    );
-    document.querySelector(".min-temperature").innerHTML = Math.round(
-      response.data.main.temp_min
-    );
-    cityLabel.innerHTML = response.data.name;
-    tempValue.innerHTML = curTemp;
-  });
+  return axios.get(apiUrl).then(uiUpdate);
+}
+
+function uiUpdate(response) {
+  let curTemp = Math.round(response.data.main.temp);
+  cityLabel.innerHTML = response.data.name;
+  tempValue.innerHTML = curTemp;
+  console.log("response", response.data);
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML =
+    Math.round(response.data.wind.speed * 3.6 * 1) / 1;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector(".max-temperature").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector(".min-temperature").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
 }
 
 function searchFieldHandler(e) {
@@ -98,7 +83,7 @@ let units = "metric";
 
 searchField.addEventListener("keypress", searchFieldHandler);
 
-getCityWeather("Kyiv");
+currentLocationHandler();
 
 // Celsius to Fahrenheit
 
